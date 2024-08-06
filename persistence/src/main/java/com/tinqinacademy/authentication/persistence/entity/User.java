96cd16e9a0fa69d825f7c8c368df6d872dd3acf9
Daniel Stanchev <148866644+DanielStanchev@users.noreply.github.com>
2024-08-06 12:28:24 +0300

    t4 - add all operations by packages in api package along with Entity needed for blacklisted tokens (#9)
    * t3 - added spring boot admin dependency and config of application properties (#5)
    
    * t4 - add all operations in api module, add spring security dependency, implement register user
    
    * t5 - add BlacklistedToken Entity and Repository

---
+++ persistence/src/main/java/com/tinqinacademy/authentication/persistence/entity/User.java
@@ -0,0 +1,47 @@
+package com.tinqinacademy.authentication.persistence.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Builder
+
+@Entity
+@Table(name = "users")
+public class User extends BaseEntity {
+
+    @Column(name = "username", nullable = false,unique = true)
+    private String username;
+
+    @Column(name = "password",nullable = false)
+    private String password;
+
+    @Column(name = "first_name",nullable = false)
+    private String firstName;
+
+    @Column(name = "last_name",nullable = false)
+    private String lastName;
+
+    @Column(name = "email", nullable = false)
+    private String email;
+
+    @Column(name = "phone_no",nullable = false,unique = true)
+    private String phoneNo;
+
+    @Column(name = "birth_date",nullable = false)
+    private LocalDate birthDate;
+
+}

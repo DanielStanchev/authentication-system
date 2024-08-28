8f5e54e55330a338ca817fb94be9fd6792910a5b
Daniel Stanchev <148866644+DanielStanchev@users.noreply.github.com>
2024-08-28 10:27:54 +0300

    t20 - fix AuthenticationRestExport authenticate URL to RestApiRoutes url

--- restexport/src/main/java/com/tinqinacademy/authentication/restexport/AuthenticationRestExport.java
+++ restexport/src/main/java/com/tinqinacademy/authentication/restexport/AuthenticationRestExport.java
@@ -2,29 +2,14 @@ package com.tinqinacademy.authentication.restexport;
 
 import com.tinqinacademy.authentication.api.operations.authenticateuser.AuthenticateUserInput;
 import com.tinqinacademy.authentication.api.operations.authenticateuser.AuthenticateUserOutput;
-import com.tinqinacademy.authentication.api.operations.checkuserage.CheckUserAgeOutput;
-import com.tinqinacademy.authentication.api.operations.loginuser.LoginUserInput;
-import com.tinqinacademy.authentication.api.operations.loginuser.LoginUserOutput;
-import com.tinqinacademy.authentication.api.operations.registeruser.RegisterUserInput;
-import com.tinqinacademy.authentication.api.operations.registeruser.RegisterUserOutput;
+import com.tinqinacademy.authentication.api.restapiroutes.RestApiRoutes;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 @FeignClient(name = "authentication")
 public interface AuthenticationRestExport {
 
-    @PostMapping("auth/authenticate")
+    @PostMapping(RestApiRoutes.AUTH_AUTHENTICATE)
     AuthenticateUserOutput authenticate(@RequestBody AuthenticateUserInput authenticateUserInput);
-
-    @PostMapping("auth/login")
-    LoginUserOutput login(@RequestBody LoginUserInput loginUserInput);
-
-    @PostMapping("auth/register")
-    RegisterUserOutput register(@RequestBody RegisterUserInput registerUserInput);
-
-    @GetMapping("auth/check/{userId}")
-    CheckUserAgeOutput checkUserAge(@PathVariable("userId") String userId);
 }
